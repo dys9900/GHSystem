7119f0adbaff15cdad2664cfeb51e59698b93a64
dys9900 <157671555@qq.com>
2017-10-12 06:43:11 +0800

    2017-10-12 6:43 Debug

---
+++ GHClinetSystem/src/com/clinet/view/TestView.java
@@ -0,0 +1,15 @@
+package com.clinet.view;
+
+import javax.swing.JFrame;
+
+public class TestView extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		
+	}
+}
